d98d484dc8a7eac48821e4b179f1efe4089983fa
chasenaidoo <chasenaidoo9@gmail.com>
2024-03-23 09:06:51 -0700

    Added sys

--- markdown2html.py
+++ markdown2html.py
@@ -10,12 +10,11 @@ Arguments:
 import argparse
 import pathlib
 import re
+import sys
 
 
 def convert_md_to_html(input_file, output_file):
-    """
-    Converts markdown file to HTML file
-    """
+    """Converts markdown file to HTML file"""
     # Read the contents of the input file
     with open(input_file, encoding='utf-8') as f:
         md_content = f.readlines()
@@ -49,9 +48,9 @@ if __name__ == '__main__':
     # Check if the input file exists
     input_path = pathlib.Path(args.input_file)
     if not input_path.is_file():
+        print(f'Missing {args.input_file}', file=sys.stderr)
         print(f'Missing {input_path}', file=sys.stderr)
         sys.exit(1)
 
     # Convert the markdown file to HTML
     convert_md_to_html(args.input_file, args.output_file)
-
